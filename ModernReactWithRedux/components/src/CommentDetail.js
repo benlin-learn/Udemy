1e6efc1edb9593ee6a552cf722a6748d7c28cf6c
wide8550 <wide8550@gmail.com>
2021-01-25 13:21:18 +0800

    get rid of warning in console

--- ModernReactWithRedux/components/src/CommentDetail.js
+++ ModernReactWithRedux/components/src/CommentDetail.js
@@ -4,11 +4,13 @@ import React from 'react';
 const CommentDetail = (props) => {
   return (
     <div className='comment'>
-      <a className='avatar'>
-        <img src={props.avatar} />
+      <a className='avatar' href='/#'>
+        <img src={props.avatar} alt='avatar' />
       </a>
       <div className='content'>
-        <a className='author'>{props.author}</a>
+        <a className='author' href='/#'>
+          {props.author}
+        </a>
         <div className='metadata'>
           <span className='date'>Today at {props.timeAgo}</span>
         </div>
