4701c01c6e2c5456d411db2647f058ac5fad4207
wide8550 <wide8550@gmail.com>
2021-01-26 09:03:31 +0800

    intro of class component , 'state' in react

---
+++ ModernReactWithRedux/seasons/src/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // THIS IS THE ONLY TIME we do direct assignment to this.state
+    this.state = {
+      lat: null,
+      errorMessage: ''
+    };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // we called setState !
+        this.setState({
+          lat: position.coords.latitude
+        });
+
+        // we did not !!
+        // this.state.lat = position.coords.latitude;
+      },
+      (err) => {
+        this.setState({
+          errorMessage: err.message
+        });
+        // console.log(err);
+      }
+    );
+  }
+  // React says we hove to define render!
+  render() {
+    // have error and do not hove latitude
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    // do not have error and hove latitude
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    // don't have error and also don't have latitude
+    return <div>Loading ...</div>;
+
+    // course didn't cover the following condition
+    // "hove both error and latitude"
+    // change first condition to :
+
+    // if (this.state.errorMessage) {
+    //   return <div>Error: {this.state.errorMessage}</div>;
+    // }
+
+    // might help
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
