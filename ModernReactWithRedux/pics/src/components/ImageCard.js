4348aa2882be054a3dd0c54b35bc0f19bb47d163
wide8550 <wide8550@gmail.com>
2021-01-28 13:14:38 +0800

    change the style grid-row-end every single image individually through react ref system

--- ModernReactWithRedux/pics/src/components/ImageCard.js
+++ ModernReactWithRedux/pics/src/components/ImageCard.js
@@ -1,11 +1,32 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { spans: 0 };
+    this.imageRef = React.createRef();
+  }
+
+  // if we try access the height of the image through componentDidMount, it's going to be way too early to get the height, because we do not yet have the image loaded up
+  // that's the reason why this.imageRef.current.clientHeight return 0
+
+  componentDidMount() {
+    // console.log(this.imageRef.current.clientHeight);
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10);
+    this.setState({ spans: spans });
+  };
+
   render() {
     const { description, urls } = this.props.image;
     return (
-      <div>
-        <img src={urls.regular} alt={description} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} src={urls.regular} alt={description} />
       </div>
     );
   }
