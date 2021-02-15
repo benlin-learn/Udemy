e28c0a8f0ce8ad52c85d5f8209ecc8bb5fbe4221
wide8550 <wide8550@gmail.com>
2021-02-15 13:06:11 +0800

    build Link Component make Routing only chanage through  difference of URL instead of rerender everything

--- ModernReactWithRedux/widgets/src/components/Route.js
+++ ModernReactWithRedux/widgets/src/components/Route.js
@@ -1,5 +1,23 @@
+import { useEffect, useState } from 'react';
+
 const Route = ({ path, children }) => {
-  return window.location.pathname === path ? children : null;
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      // console.log('Location Change');
+      setCurrentPath(window.location.pathname);
+    };
+
+    window.addEventListener('popstate', onLocationChange);
+
+    // cleanup eventListener
+    return () => {
+      window.removeEventListener('popstate', onLocationChange);
+    };
+  }, []);
+
+  return currentPath === path ? children : null;
 };
 
 export default Route;
