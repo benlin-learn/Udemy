3474c4a401e37209f0f74d5d9b1d9841f67b8d8e
wide8550 <wide8550@gmail.com>
2021-02-20 15:52:28 +0800

    refactor - with lodash _.chain

--- ModernReactWithRedux/blog/src/actions/index.js
+++ ModernReactWithRedux/blog/src/actions/index.js
@@ -15,10 +15,15 @@ export const fetchPostsAndUsers = () => {
     // console.log('fetched posts!');
 
     // console.log(getState().posts);
-    const userIds = _.uniq(_.map(getState().posts, 'userId'));
 
-    userIds.forEach((id) => dispatch(fetchUser(id)));
-    // await dispatch(fetchUser());
+    // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    // userIds.forEach((id) => dispatch(fetchUser(id)));
+
+    _.chain(getState().posts)
+      .map('userId')
+      .uniq()
+      .forEach((id) => dispatch(fetchUser(id)))
+      .value();
   };
 };
 
